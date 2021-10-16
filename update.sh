1bef824cf6b941a3799cde56335165532291815a
Mednikov Yuriy <mednikov.yuriy@gmail.com>
2021-10-16 12:44:48 +0000

    Update database

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
+set -x;
+
 python3 update_credits.py;
 python3 update_onboarding.py;
 python3 update_states.py;
 python3 update_clusters.py;
+python3 update_validators_testnet.py;
+python3 update_signups.py;
 python3 generate_static.py;
+
+set +x;
